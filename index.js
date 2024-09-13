e6c5c4246504100fc637cbc9b2faff6a3d1e7c07
João Marcelo Barros Vilas Boas <joaombvb@gmail.com>
2024-09-13 14:07:24 -0300

    save

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
-const { select } = require('@inquirer/prompts');
+const { select, input, checkbox } = require('@inquirer/prompts');
+
+let meta = {
+    value: "Meta de teste",
+    checked: false,
+}
+
+let metas = [ meta ];
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a sua meta:"});
+
+    if(meta.length == 0) {
+        console.log("A meta não pode ser vazia.");
+        return;
+    }
+
+    metas.push({ value: meta, checked : false })
+}
+
+const listarMetas =  async() => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas]
+    })
+
+    if(respostas.length == 0) {
+        return;
+    }
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta;
+        })
+
+        meta.checked = true;
+    })
+}
 
 const start = async () => {
 
@@ -24,15 +61,16 @@ const start = async () => {
 
         switch(opcao) {
             case "cadastrar":
-                console.log("sexo")
-                break
+                await cadastrarMeta();
+                console.log(metas);
+                break;
             case "listar":
-                break
+                break;
             case "sair":
-                return
+                return;
         }
     }
 
 }
 
-start()
+start();
